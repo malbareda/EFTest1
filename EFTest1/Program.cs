1056c2c67ef0eded856b19a226e5d48ff50b9555
malbareda <dm.albareda@gmail.com>
2018-12-07 07:53:44 +0100

    Agregar archivos de proyecto.

---
+++ EFTest1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jaume" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+}
